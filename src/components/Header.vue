e8e0a74540a759ce1b1e9814153c3f328812a05e
mirkettinho <mirkodifranco30@gmail.com>
2023-04-28 09:28:59 +0200

    [add] header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,40 @@ export default {
 </script>
 
 <template>
-  <div class="container ht-100">
-  </div>
+  <header class="container">
+    <div class="logo">
+      <!-- IMG IN PERCORSO ASSOLUTO PERCHè SI TROVA IN PUBLIC -->
+      <img src="/dc-logo.png" alt="Logo DC">
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="#">characters</a></li>
+        <li><a href="#">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">video</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use "../scss/general/mixin" as *;
 
+header{
+  
+  ul{
+    @include centerFlex("horizontal");
+    a{
+      padding: 10px;
+      text-transform: uppercase;
+    }
+  }
+}
 </style>
